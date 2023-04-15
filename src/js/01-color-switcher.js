bd211e5a2239672e085365c23f5f72f3321a7997
Anastasia27041 <anastasiaglyvka@gmail.com>
2023-04-15 17:19:57 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('[data-start');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+let id = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onClickStart() {
+  id = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', '');
+}
+
+function onClickStop() {
+  clearInterval(id);
+  btnStart.removeAttribute('disabled');
+}
